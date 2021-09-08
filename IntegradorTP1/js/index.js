fd460b827771f846455da0e95cbb3fa67ca3b311
nahuelflores <arcangel6191@gmail.com>
2021-09-08 18:55:19 -0300

    Borrado de filtro gris en indexjs

--- IntegradorTP1/js/index.js
+++ IntegradorTP1/js/index.js
@@ -35,28 +35,6 @@ function oMousePos(canvas, evt) {
   }
 }
 
-/** SECCCION DE FILTROS */
-function filtroGris(){
-  // APLICADO DE FILTRO -> ESCALA GRISES
-  let cimg = document.getElementById("canvas");
-  let ctximg = cimg.getContext('2d');
-  let data_image = ctximg.getImageData(0,0,cimg.width,cimg.height);
-  for (let fila = 0; fila < cimg.width; fila++) {
-    for (let columna = 0; columna < cimg.height; columna++) {
-      // calculo el color gris del pixel
-      let grey = (getRed(data_image,fila,columna)+getBlue(data_image,fila,columna)+getGreen(data_image,fila,columna))/3
-      // Modify pixel data
-      let index = (fila+columna*data_image.width)*4;
-      data_image.data[index + 0] = grey;  // R value
-      data_image.data[index + 1] = grey;    // G value
-      data_image.data[index + 2] = grey;  // B value
-      data_image.data[index + 3] = 255;  // A value
-    }
-  }
-  ctximg = document.getElementById("canvas").getContext('2d');
-  ctximg.putImageData(data_image,0,0);
-}
-
 /// OBTENCION DE PIXEL-COLORS PARA IMAGEDATA
 function getRed(imageData,x,y){
   let index = (x+y*imageData.width)*4;
