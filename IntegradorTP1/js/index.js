2da9fa750b2441912b7060a941b7e012a10cc3fd
nahuelflores <arcangel6191@gmail.com>
2021-09-08 18:54:22 -0300

    Ajustado de imagen al canvas en caso de que sean mas grandes que el mismo,guardado de imagen, seteo de lapiz y borrado con sus respectivos colores, agreagado de color picker para pintar.

--- IntegradorTP1/js/index.js
+++ IntegradorTP1/js/index.js
@@ -2,13 +2,13 @@ let image;
 let cimg = document.getElementById("canvas");
 let ctximg = cimg.getContext('2d');
 let dibujar = false;
+let configPincel = true;
+let configBorrado = false;
 
 document.addEventListener("DOMContentLoaded", async function() {
 
-  // await loadImage('../omnimon.jpg');
   cimg.addEventListener('mousedown', function(evt) {
     dibujar = true;
-    // ctximg.clearRect(0, 0, cimg.width, cimg.height);
     ctximg.beginPath();
   
   }, false);
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", async function() {
 
 function oMousePos(canvas, evt) {
   var ClientRect = canvas.getBoundingClientRect();
-  return { //objeto
+  return { 
     x: Math.round(evt.clientX - ClientRect.left),
     y: Math.round(evt.clientY - ClientRect.top)
   }
@@ -55,11 +55,6 @@ function filtroGris(){
   }
   ctximg = document.getElementById("canvas").getContext('2d');
   ctximg.putImageData(data_image,0,0);
-}
-
-/* Carga imagen desde PC*/
-function cargarImagen(){
-
 }
 
 /// OBTENCION DE PIXEL-COLORS PARA IMAGEDATA
@@ -95,13 +90,24 @@ function getBlue(imageData,x,y){
 //  }
 // }
 
+/**
+ * Carga de imagen mediante funcion readAsDataURL
+ * @param file id myFile en html para tomar los datos de la imagen a cargar. NO SE RECIBE COMO PARAMETRO, se lo consigue desde
+ * el documento.
+ */
+
 function loadImage() {
+  nuevoCanvas();
   let file, fr;
   let input = document.getElementById('myFile');
   file = input.files[0];
   fr = new FileReader();
-  fr.onload = createImage;
-  fr.readAsDataURL(file);
+  try{
+    fr.onload = createImage;
+    fr.readAsDataURL(file);
+  } catch(e){
+    alert('Imagen no seleccionada');
+  }
 
   function createImage() {
       image = new Image();
@@ -119,13 +125,60 @@ function loadImage() {
     if (image.width*ratio < canvas.width){
       ctximg.canvas.height = image.width*ratio;
     }
-    ctximg.drawImage(image,0,0);
+    ctximg.drawImage(image,0,0,cimg.width,cimg.height);
   }
 
   input.value = "";
   toggleNavbar();
 }
 
+/**
+ * Toggle de clase show para que la navegacion se retraiga para comodidad del usuario
+ */
 function toggleNavbar(){
   document.getElementById('navbarHeader').classList.toggle('show');
+}
+/**
+ * Dibujado de rectangulo vacio en el canvas para simular hoja nueva.
+ */
+function nuevoCanvas(){
+  ctximg.clearRect(0, 0, cimg.width, cimg.height);
+  ctximg.fillRect(0, 0, cimg.width, cimg.height);
+  cimg.width = 1100;
+  cimg.height = 550;
+  toggleNavbar();
+}
+/**
+ * Guardado de datos actuales del canvas a imagen png en la pc
+ */
+function guardarImagen(){
+  let link = document.createElement('a');
+  link.download = 'ImagenCanvasDownload.png';
+  link.href = cimg.toDataURL()
+  link.click();
+}
+
+/**
+ * Funcion para setear la configuarcion del Pincel
+ */
+function setPincel(){
+  ctximg.strokeStyle = "#000000";
+}
+/**
+ * Funcion para setear la configuarcion de la goma de borrado
+ */
+function setBorrado(){
+  ctximg.strokeStyle = "#FFFFFF";
+}
+
+function setStrokeWidth(){
+  ctximg.lineWidth = 1;
+}
+/**
+ * Funcion para cambiar de color
+ * Busca el input con el valor RGB para asignar.
+ */
+function setStrokeColor(){
+  let color = document.getElementById('color_picker').value;
+  ctximg.strokeStyle = color;
 }
