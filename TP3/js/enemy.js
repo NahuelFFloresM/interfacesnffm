92fc2d9006b15e484585ad09503580bcd53c24fc
Pablo Cutropía <pablo.cutro@gmail.com>
2021-10-27 21:14:39 -0300

    enemy styles agregados. modificaciones enemy.js e index.js para manejar las clases nuevas para los enemigos

--- TP3/js/enemy.js
+++ TP3/js/enemy.js
@@ -6,32 +6,36 @@ class Enemy{
   #bottom_pos;
   #div_item;
   #id;
+  #div_class;
 
-  constructor(w,h,bs,rp,bp,id){
+  constructor(w,h,bs,rp,bp,id,div_class){
     this.#width = w;
     this.#height = h;
     this.#bottom_pos = bs;
     this.#right_pos = rp;
     this.#background_sprite = bp;
     this.#id = id;
+    this.#div_class = div_class;
   }
   /**
    * Crea el div con sus parametros y hace "append" al documento
    */
   spawn(){
+    console.log(this.#div_class);
     let box = document.createElement('div');
     this.#div_item = box;
     // box.style.width='100px';
-    box.style.width= this.#width;
+    //box.style.width= this.#width;
     // box.style.height='100px';
-    box.style.height= this.#height;
-    box.style.backgroundColor='red';
+    //box.style.height= this.#height;
+    //box.style.backgroundColor='red';
     box.style.position = 'absolute';
     // box.style.right = '20px';
     box.style.right = this.#right_pos;
     // box.style.bottom = '5%';
     box.style.bottom = this.#bottom_pos;
     box.id = this.#id;
+    box.classList.add(this.#div_class);
     // ------------------------ DONDE LOS SPAWNEA EN LA VENTANA
     document.body.appendChild(box);
   }
