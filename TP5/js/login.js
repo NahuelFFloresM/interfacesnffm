2a333ca1308dac0bb5518eabeedbc7d3080d2acd
Ignacio Miguez <Ignacio.Miguez@tsoftglobal.com>
2021-12-01 01:58:33 -0300

    Login and Sign Up created

---
+++ TP5/js/login.js
@@ -0,0 +1,6 @@
+document.getElementById("register-btn").onclick = () => {
+    // window.location.href = ""; // loading page
+    setTimeout(() => {
+        window.location.href = "./sign-up.html";
+    }, 2000);
+}
---
+++ TP5/js/sign-up.js
@@ -0,0 +1,6 @@
+document.getElementById("login-link").onclick = () => {
+    // window.location.href = ""; // loading page
+    setTimeout(() => {
+        window.location.href = "./login.html";
+    }, 2000);
+}
