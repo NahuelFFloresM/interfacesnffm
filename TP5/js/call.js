fb34fcd6ea90632050aeea8fd0a589ffd3cb0e3f
Pablo Cutropía <pablo.cutro@gmail.com>
2021-12-03 16:12:54 -0300

    Agregado timer durante la llamada

--- TP5/js/call.js
+++ TP5/js/call.js
@@ -1,3 +1,23 @@
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
+var totalSeconds = 0;
+setInterval(setTime, 1000);
+
+function setTime() {
+  ++totalSeconds;
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+  var valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
+
 setTimeout(function(){ 
     document.getElementById("beater").classList.remove("beat");
 }, 5000);
